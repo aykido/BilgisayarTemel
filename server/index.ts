fca07247bea7c00eda64332884f034c5ba3e20ae
aykido <103534496+aykido@users.noreply.github.com>
2025-09-10 20:00:33 +0300

    Feat: Dynamic host and port configuration

--- server/index.ts
+++ server/index.ts
@@ -115,12 +115,14 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 3005;
+  const HOST = process.env.NODE_ENV === 'development' ? 'localhost' : '0.0.0.0';
+  const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3005;
+
   server.listen({
-    port,
-    host: "0.0.0.0",
+    port: PORT,
+    host: HOST,
     reusePort: true,
   }, () => {
-    log(`serving on port ${port}`);
+    log(`serving on ${HOST}:${PORT}`);
   });
 })();
